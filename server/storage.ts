5b8c8671c39abb7a672518b8867b32cc32970c8b
Hamza Charaf-Eddine <hamzacharafeddine77@gmail.com>
2025-09-05 01:41:37 +0100

    update v2

--- server/storage.ts
+++ server/storage.ts
@@ -13,7 +13,7 @@ import type {
   InsertReview,
   BookingWithActivity,
   ReviewWithActivity,
-} from "../shared/schema";
+} from "@shared/schema";
 
 // MongoDB connection string - must use DATABASE_URL
 const DATABASE_URL = process.env.DATABASE_URL;
