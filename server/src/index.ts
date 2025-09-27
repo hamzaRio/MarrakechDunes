aebf8ef38fd4044bded5f147654e70c3ed2815f5
Hamza Charaf-Eddine <hamzacharafeddine77@gmail.com>
2025-09-27 11:55:47 +0100

    Fix TypeScript errors in global error handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -370,7 +370,7 @@ app.use((req, res, next) => {
   });
 
   // Global error handler (must be last)
-  app.use((err, req, res, next) => {
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     console.error("Unhandled error:", err);
     res.status(500).json({
       error: "Internal server error",
